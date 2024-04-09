2c9a09dd8f292d95b792a3774943ff1da64ee2fd
rahulio96 <rahgupta96@gmail.com>
2024-04-08 17:41:23 -0700

    Basic navbar and home components

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../navbar/Navbar"
+
+function Home() {
+    return (
+        <>
+            <Navbar />
+            <div>
+                home content
+            </div>
+        </>
+    )
+}
+
+export default Home
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import navbarCSS from './Navbar.module.css'
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href=''>Home</a></li>
+                <li><a href=''>Create</a></li>
+                <li><a href=''>Gallery</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
