867eb6dad9ee6cf11a446fa4e057e366d4e293f1
rahulio96 <rahgupta96@gmail.com>
2024-04-08 20:45:01 -0700

    Added Home page and navbar adjustments

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,25 @@
 import Navbar from "../navbar/Navbar"
+import homeCSS from './Home.module.css'
 
 function Home() {
     return (
         <>
             <Navbar />
-            <div>
-                home content
+            <div className={homeCSS.container}>
+                <img className={homeCSS.logo} src="vault-tec-logo.png" />
+                <h1 className={homeCSS.title}>Welcome to Vault 98</h1>
+                <div className={homeCSS.terminal}>
+                    <div className={homeCSS.sysInfo}>
+                        <p>ROBCO INDUSTRIES UNIFIED OPERATING SYSTEM</p>
+                        <p>COPYRIGHT 2075-2077 ROBCO INDUSTRIES</p>
+                        <p>-Server 7-</p>
+                    </div>
+                    <p>Welcome, esteemed Overseer, to the genesis of your own Vault-Tec experiment! 
+                    Here, you'll craft your cohort of intrepid vault dwellers, ready to brave the treacherous 
+                    wasteland beyond our vault's safety!</p>
+                    <hr />
+                    <button className={homeCSS.button}>{">"} CLICK HERE TO GET STARTED</button>
+                </div>
             </div>
         </>
     )
