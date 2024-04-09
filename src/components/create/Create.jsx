20a05ebe44b427f3925d455705f9ab9f07096177
rahulio96 <rahulgithub96@gmail.com>
2024-04-09 15:39:40 -0700

    Added default files for gallery and create components

---
+++ src/components/create/Create.jsx
@@ -0,0 +1,11 @@
+import homeCSS from "../home/Home.module.css"
+
+function Create() {
+    return (
+        <>
+            <h1>Create a Vault Dweller</h1>
+        </>
+    )
+}
+
+export default Create
