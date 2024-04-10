dff29ee59fa402792f2bf706edee7103552f0bef
rahulio96 <rahgupta96@gmail.com>
2024-04-10 12:54:01 -0700

    Added stat numbers to create screen

--- src/components/create/Create.jsx
+++ src/components/create/Create.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react"
 import homeCSS from "../home/Home.module.css"
 import createCSS from "./Create.module.css"
 
 function Create() {
+    const [str, setStr] = useState(1)
+    const [per, setPer] = useState(1)
+    const [end, setEnd] = useState(1)
+    const [cha, setCha] = useState(1)
+    const [int, setInt] = useState(1)
+    const [agi, setAgi] = useState(1)
+    const [luc, setLuc] = useState(1)
+
+    /*
+        "Endurance" : end,
+        "Charisma" : cha,
+        "Intelligence" : int,
+        "Agility" : agi,
+        "Luck" : luc
+    */
+    let statList = [
+        ["Strength", str],
+        ["Perception", per],
+        ["Endurance", end],
+        ["Charisma", cha],
+        ["Intelligence", int],
+        ["Agility", agi],
+        ["Luck", luc]
+
+    ]
+
     return (
         <>
         <div className={homeCSS.container}>
@@ -14,16 +41,22 @@ function Create() {
                 </div>
                 <p>Before venturing into the Wasteland, ensure your Vault Dweller is properly prepared. 
                 Utilize our cutting-edge genetic modification technology to customize your Dweller for optimal survival. 
-                Remember, a well-prepared Vault Dweller is a safer Vault Dweller.</p>
-                <img className={createCSS.boy} src="vault-boy.jpg"/>
+                Remember, a well-prepared Vault Dweller is a safer Vault Dweller.</p>        
                 <hr />
-                <button className={homeCSS.button}>{">"} Strength</button>
-                <button className={homeCSS.button}>{">"} Perception</button>
-                <button className={homeCSS.button}>{">"} Endurance</button>
-                <button className={homeCSS.button}>{">"} Charisma</button>
-                <button className={homeCSS.button}>{">"} Intelligence</button>
-                <button className={homeCSS.button}>{">"} Agility</button>
-                <button className={homeCSS.button}>{">"} Luck</button>
+                <div className={createCSS.stats}>
+                    <div className={createCSS.btnContainer}>
+
+                    {statList.map((stat, index) => 
+                        
+                        <div key={index} className={createCSS.button}>
+                            {stat[0]}
+                            <div className={createCSS.number}>{stat[1]}</div>
+                        </div>
+                    )}
+                    
+                    </div>
+                    <img className={createCSS.boy} src="vault-boy-2.png"/>
+                </div>
             </div>
         </div>
         </>
